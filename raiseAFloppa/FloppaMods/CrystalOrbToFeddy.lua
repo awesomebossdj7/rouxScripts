d996a00425d149dbed2fadc52b01664d66537fbd
OwOsucksAwesomeMinecraftbossAlissha6TRM <70352423+OwOsucksAwesomeMinecraftbossAlissha6TRM@users.noreply.github.com>
2022-05-11 01:36:06 -0400

    Create CrystalOrbToFeddy.lua

---
+++ raiseAFloppa/FloppaMods/CrystalOrbToFeddy.lua
@@ -0,0 +1,6 @@
+local CB = game.Workspace["Crystal Ball"]
+
+CB.Size =  Vector3.new(0.3, 0.4, 0.3)
+CB.Rotation =  Vector3.new(180, 0, -180)
+CB.MeshId = 'rbxassetid://511715966'
+CB.TextureID = 'rbxassetid://511716115'
