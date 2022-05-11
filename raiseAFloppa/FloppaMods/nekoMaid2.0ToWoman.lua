7d62449c77cd89ca3e2a2520a4be5a60534b5974
OwOsucksAwesomeMinecraftbossAlissha6TRM <70352423+OwOsucksAwesomeMinecraftbossAlissha6TRM@users.noreply.github.com>
2022-05-11 01:37:12 -0400

    Add files via upload
    women

---
+++ raiseAFloppa/FloppaMods/nekoMaid2.0ToWoman.lua
@@ -0,0 +1,10 @@
+game:GetService("Workspace")["Neko Maid 2.0"]["Black Messy Cat Boy Hair"].Handle.SpecialMesh.MeshId = 'rbxassetid://9180805590'
+game:GetService("Workspace")["Neko Maid 2.0"]["Black Messy Cat Boy Hair"].Handle.SpecialMesh.Offset = Vector3.new(0, 0, 0.1)
+game:GetService("Workspace")["Neko Maid 2.0"]["Black Messy Cat Boy Hair"].Handle.SpecialMesh.TextureId = 'rbxassetid://9180805666'
+
+game:GetService("Workspace")["Neko Maid 2.0"]["Superhero Right Arm"].MeshId = '83001181'
+game:GetService("Workspace")["Neko Maid 2.0"]["Superhero Left Arm"].MeshId = '83001137'
+game:GetService("Workspace")["Neko Maid 2.0"]["Superhero Right Leg"].MeshId = '81628308'
+game:GetService("Workspace")["Neko Maid 2.0"]["Superhero Left Leg"].MeshId = '81628361'
+game:GetService("Workspace")["Neko Maid 2.0"]["Superhero Torso"].MeshId = '82987757'
+game:GetService("Workspace")["Neko Maid 2.0"].Head.face.Texture = 'http://www.roblox.com/asset/?id=2067383048'
